af483bbb954c9baf0eef3266b2620c54bd710fd9
Jasjeev <jasjeev99@gmail.com>
2024-12-30 23:34:29 +0530

    Add video streaming functionality with ReactPlayer and update PeerProvider for remote stream handling

--- client/src/pages/Room.jsx
+++ client/src/pages/Room.jsx
@@ -1,15 +1,20 @@
-import React, { useEffect,useCallback } from 'react'
+import React, { useEffect,useCallback,useState } from 'react'
+import ReactPlayer from 'react-player'
 import { useSocket } from '../providers/Socket'
 import { usePeer } from '../providers/Peer'
 
 function Room() {
     const { socket } = useSocket()
-    const { peer,createOffer,createAnswer,setRemoteAns } = usePeer()
+    const { peer,createOffer,createAnswer,setRemoteAns,sendStream,remoteStream } = usePeer()
+
+    const [myStream, setMyStream] = useState(null);
+    const [remoteEmailId, setRemoteEmailId] = useState(null);
 
     const handleNewUserJoined =useCallback( async ({ emailId }) => {
         console.log(`${emailId} joined room`);
         const offer = await createOffer();
         socket.emit('call-user', { emailId, offer });
+        setRemoteEmailId(emailId);
 
     },[createOffer,socket])
 
@@ -17,13 +22,27 @@ function Room() {
         console.log(`${from} is calling you`);
         const ans = await createAnswer(offer);
         socket.emit('call-accepted',{emailId:from,ans});
+        setRemoteEmailId(from)
     },[createAnswer,socket]);
 
     
     const handleCallAccepted = useCallback(async ({ ans }) => {
         console.log('Call accepted: ',ans);
         await setRemoteAns(ans);
+        
+
     },[setRemoteAns]);
+
+    const getUserMediaStream = useCallback(async () => {
+        const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+        setMyStream(stream);
+
+    },[])
+
+    const handleNegotiationNeededEvent = useCallback(async () => {
+        const localOffer = peer.createOffer();
+        socket.emit('call-user',{emailId:remoteEmailId,offer:localOffer});
+    },[peer,remoteEmailId,socket])
 
 
     useEffect(() => {
@@ -31,16 +50,31 @@ function Room() {
         socket.on('incoming-call',handleIncomingCall);
         socket.on('call-accepted',handleCallAccepted);
 
-        return () => {
-            socket.off('user-joined',handleNewUserJoined);
-            socket.off('incoming-call',handleIncomingCall);
-            socket.off('call-accepted',handleCallAccepted);
-        }
+        // return () => {
+        //     socket.off('user-joined',handleNewUserJoined);
+        //     socket.off('incoming-call',handleIncomingCall);
+        //     socket.off('call-accepted',handleCallAccepted);
+        // }
     },[handleCallAccepted,handleNewUserJoined,handleIncomingCall,socket])
+
+    useEffect(() => {
+    
+            peer.addEventListener('negotiationneeded',handleNegotiationNeededEvent)
+            return () => peer.removeEventListener('negotiationneeded',handleNegotiationNeededEvent)
+    },[handleNegotiationNeededEvent,peer])
+
+    useEffect(() => {
+        getUserMediaStream();
+    },[getUserMediaStream])
 
     return (
         <div className='room-page-container'>
             <h1>Room Page</h1>
+            <h4>You are connected to {remoteEmailId}</h4>
+            <button onClick={e => sendStream(myStream)}>Send My Video</button>
+            <ReactPlayer url={myStream} playing={true} controls={true} width='50%' height='50%'/>
+            <ReactPlayer url={remoteStream} playing={true} controls={true} width='50%' height='50%'/>
+
         </div>
     )
 }
--- client/src/providers/Peer.jsx
+++ client/src/providers/Peer.jsx
@@ -1,10 +1,12 @@
-import React,{useMemo} from "react";
+import React, { useMemo, useEffect, useState, useCallback } from "react";
 
 const PeerContext = React.createContext(null);
 
 export const usePeer = () => React.useContext(PeerContext);
 
 export const PeerProvider = (props) => {
+    const [remoteStream, setRemoteStream] = useState(null);
+
     const peer = useMemo(() => {
         return new RTCPeerConnection({
             iceServers: [
@@ -18,7 +20,7 @@ export const PeerProvider = (props) => {
                 },
             ],
         });
-    },[])
+    }, [])
 
     const createOffer = async () => {
         const offer = await peer.createOffer();
@@ -26,7 +28,7 @@ export const PeerProvider = (props) => {
         return offer;
     }
 
-    const createAnswer  = async (offer) => {
+    const createAnswer = async (offer) => {
         await peer.setRemoteDescription(offer);
         const answer = await peer.createAnswer();
         await peer.setLocalDescription(answer);
@@ -37,8 +39,29 @@ export const PeerProvider = (props) => {
         await peer.setRemoteDescription(ans);
     }
 
+    const sendStream = async (stream) => {
+        const tracks = stream.getTracks();
+        for (const track of tracks) {
+            peer.addTrack(track, stream);
+        }
+    }
+
+    const handleTrackEvent = useCallback((ev) => {
+        const streams = ev.streams;
+        setRemoteStream(streams[0]);
+    }, [])
+
+    
+
+    useEffect(() => {
+        peer.addEventListener("track", handleTrackEvent)
+        return () => {
+            peer.removeEventListener('track', handleTrackEvent)
+        }
+    }, [peer, handleTrackEvent])
+
     return (
-        <PeerContext.Provider value={{peer,createOffer,createAnswer,setRemoteAns}}>
+        <PeerContext.Provider value={{ peer, createOffer, createAnswer, setRemoteAns, sendStream, remoteStream }}>
             {props.children}
         </PeerContext.Provider>
     )
